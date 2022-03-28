238acccb66fb8d3bd4f33bd5316b072942f38581
Jake Mangan <jakemangan1@hotmail.co.uk>
2022-03-28 18:53:42 +0100

    CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { WebScrapingService } from './services/web-scraping/web-scraping.service
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: 'https://dexreview.xyz',
+        origin: ['https://dexreview.xyz', 'http://localhost:3000', 'http://localhost:3001'],
     });
     await app.listen(process.env.PORT || 5050);
 
